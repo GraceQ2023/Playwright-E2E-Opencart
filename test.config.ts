5524beaf0947b8025791229ea4a4b822689f3e22
Grace Qin <s3988656@student.rmit.edu.au>
2025-11-05 11:01:10 +1100

    init: base setup for Opencart playwright automation project

---
+++ test.config.ts
@@ -0,0 +1,30 @@
+
+export class TestConfig{
+
+// Application URL
+    static readonly appUrl = 'http://localhost/opencart';
+
+// User credentials 
+    static readonly validUser = {
+        email: 'grace.test@gmail.com',
+        password: 'test1234'
+    };
+
+// Product info
+
+static readonly product = {
+    name: 'iMac',
+    quantity: 3,
+    totalPrice: '$366.00'
+};
+
+// API & DB configs
+    static readonly apiEndpoint = 'http://localhost/opencart/api';
+    static readonly dbConfig = {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'openshop'
+    };
+
+}
