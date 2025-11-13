fdeaa9b0a97957e2d3882da8fcb75d0be085ce4a
Grace Qin <s3988656@student.rmit.edu.au>
2025-11-13 11:53:25 +1100

    fix(config): update appUrl to 127.0.0.1 for CI execution

--- test.config.ts
+++ test.config.ts
@@ -2,14 +2,20 @@
 export class TestConfig{
 
 // Application URL
-    static readonly appUrl = 'http://localhost/opencart';
+    // static readonly appUrl = 'http://localhost/opencart';
 
-// User credentials 
+    // loopback ip address for localhost, works for both local and CI environments
+    static readonly appUrl ='http://127.0.0.1/opencart/'; 
+
+
+    // Valid user credentials 
     static readonly validUser = {
         email: 'grace.test@gmail.com',
         password: 'test1234'
     };
 
+
+    // Valid user credentials - new user for edit account tests
     static readonly newUser = {
         fName: 'test17',
         lName: 'test17',
@@ -18,21 +24,23 @@ export class TestConfig{
         password: 'test'
     };
 
+
+    // Invalid user credentials for negative testing
      static readonly invalidUser = {
         email: 'invalid.user@gmail.com',
         password: 'test@1'
     };
 
-// Product info
 
-static readonly product = {
-    name: 'iMac',
-    quantity: 3,
-    totalPrice: '$366.00'
-};
+    // Product details for product related tests (search, add to cart, checkout)
+    static readonly product = {
+        name: 'iMac',
+        quantity: 3,
+        totalPrice: '$366.00'
+    };
 
 
-// DB configs
+    // DB configs
     // static readonly apiEndpoint = 'http://localhost/opencart/api';
     static readonly dbConfig = {
         dbHost: 'localhost',
